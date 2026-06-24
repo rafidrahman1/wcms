3f2b1f467fb218aeaed83ce3381a711bb7cfe65e
rafidrahman <rafid0001+github@gmail.com>
2026-06-24 11:47:27 +0600

    printing works on h10s device

--- lib/core/utils/pda_printer.dart
+++ lib/core/utils/pda_printer.dart
@@ -3,84 +3,37 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:senraise_printer/senraise_printer.dart';
-import 'package:sunmi_printer_plus/sunmi_printer_plus.dart';
 
-final _senraisePrinter = SenraisePrinter();
+final _printer = SenraisePrinter();
 const _timeout = Duration(seconds: 8);
 
 Future<T> _withTimeout<T>(Future<T> future) => future.timeout(_timeout);
 
-enum PrinterType { none, senraise, sunmi }
-
-Future<PrinterType> getAvailablePrinter() async {
-  if (!Platform.isAndroid) return PrinterType.none;
-
-  // Try Sunmi first
-  try {
-    final bool isSunmi = await SunmiPrinterPlus().rebindPrinter();
-    if (isSunmi) {
-      return PrinterType.sunmi;
-    }
-  } catch (_) {}
-
-  // Try Senraise
-  try {
-    await _withTimeout(_senraisePrinter.setAlignment(1));
-    return PrinterType.senraise;
-  } catch (_) {}
-
-  return PrinterType.none;
-}
-
 Future<bool> isPdaPrinterAvailable() async {
-  return await getAvailablePrinter() != PrinterType.none;
+  if (!Platform.isAndroid) return false;
+  try {
+    await _withTimeout(_printer.setAlignment(1));
+    return true;
+  } on TimeoutException {
+    return false;
+  } on PlatformException {
+    return false;
+  }
 }
 
 Future<void> printPdaLabel({required Uint8List qrImageBytes, required List<String> lines}) async {
-  final printerType = await getAvailablePrinter();
-
-  if (printerType == PrinterType.sunmi) {
-    await _printSunmi(qrImageBytes, lines);
-  } else if (printerType == PrinterType.senraise) {
-    await _printSenraise(qrImageBytes, lines);
-  } else {
-    throw StateError('No PDA printer available');
-  }
-}
-
-Future<void> _printSunmi(Uint8List qrImageBytes, List<String> lines) async {
   try {
-    final sunmi = SunmiPrinterPlus();
-
-    await sunmi.printImage(qrImageBytes, align: SunmiPrintAlign.CENTER);
-    await sunmi.lineWrap(times: 1);
+    await _withTimeout(_printer.setAlignment(1));
+    await _withTimeout(_printer.printPic(qrImageBytes));
+    await _withTimeout(_printer.nextLine(1));
+    await _withTimeout(_printer.setTextSize(22));
+    await _withTimeout(_printer.setTextBold(false));
 
     for (final line in lines) {
-      await sunmi.printText(
-        text: line,
-        style: SunmiTextStyle(fontSize: 22, align: SunmiPrintAlign.CENTER, bold: false),
-      );
+      await _withTimeout(_printer.printText('$line\n'));
     }
 
-    await sunmi.lineWrap(times: 100);
-  } catch (e) {
-    throw StateError('Sunmi print failed: $e');
-  }
-}
-
-Future<void> _printSenraise(Uint8List qrImageBytes, List<String> lines) async {
-  try {
-    await _withTimeout(_senraisePrinter.setAlignment(1));
-    await _withTimeout(_senraisePrinter.printPic(qrImageBytes));
-    await _withTimeout(_senraisePrinter.nextLine(1));
-    await _withTimeout(_senraisePrinter.setTextSize(22));
-    await _withTimeout(_senraisePrinter.setTextBold(false));
-
-    for (final line in lines) {
-      await _withTimeout(_senraisePrinter.printText('$line\n'));
-    }
-
-    await _withTimeout(_senraisePrinter.nextLine(12));
+    await _withTimeout(_printer.nextLine(6));
   } on TimeoutException {
     throw StateError('Printer service did not respond. Check that HardWare Settings print test works.');
   } on PlatformException catch (error) {
